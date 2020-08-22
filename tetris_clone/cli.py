084dcc280b5add5c00a57be5fba75e7747bd635c
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-08-22 17:36:27 +0530

    Color Added

--- tetris_clone/cli.py
+++ tetris_clone/cli.py
@@ -8,3 +8,14 @@ import traceback
 from tetris_clone import core
 
 
+COLORS = {
+    'I': curses.COLOR_RED,
+    'O': curses.COLOR_BLUE,
+    'T': curses.COLOR_YELLOW,
+    'L': curses.COLOR_MAGENTA,
+    'J': curses.COLOR_WHITE,
+    'S': curses.COLOR_GREEN,
+    'Z': curses.COLOR_CYAN,
+}
+
+
