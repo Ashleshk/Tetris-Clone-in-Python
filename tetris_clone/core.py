5eb70297c5054cc64cb3ae82256c1625f24983df
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-08-22 17:39:44 +0530

    Shapes Array Modified

--- tetris_clone/core.py
+++ tetris_clone/core.py
@@ -10,7 +10,15 @@ HEIGHT = 20
 # that the shape rotates around and top center of the game when the
 # shape is added to it
 # y is like in math, so more y means higher
-
+SHAPES = {
+    'I': [(0, 2), (0, 1), (0, 0), (0, -1)],
+    'O': [(0, 0), (0, 1), (-1, 0), (-1, 1)],
+    'T': [(-1, 0), (0, 0), (1, 0), (0, -1)],
+    'L': [(0, 1), (0, 0), (0, -1), (1, -1)],
+    'J': [(0, 1), (0, 0), (0, -1), (-1, -1)],
+    'S': [(1, 1), (0, 1), (0, 0), (-1, 0)],
+    'Z': [(-1, 1), (0, 1), (0, 0), (1, 0)],
+}
 
 
 class Block:
